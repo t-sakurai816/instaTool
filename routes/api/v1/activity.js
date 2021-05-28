c90d258beaa401e7b93d46f63761834abbbf6851
t-sakurai816 <poppun_0816@icloud.com>
2021-05-28 16:06:27 +0900

    フォロワー数と投稿数をapiから取得

--- routes/api/v1/activity.js
+++ routes/api/v1/activity.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const dayjs = require('dayjs');
+const app = express();
 const router = express.Router();
+const env = require('./../../../env.json')[app.get('env')];
+const request = require('request');
 
 /* サンプルAPI① 
  * http://localhost:3000/api/v1/activity にGETメソッドのリクエストを投げると、
@@ -27,6 +29,26 @@ router.post('/', function (req, res, next) {
   res.send(responseDataJson);
 });
 
+
+// アカウント名をPOSTすると、現在のフォロー数と投稿数を返す
+router.post('/get-num', function (req, res, next) {
+  const accountName = req.body.accountName;
+  const URL = "https://graph.facebook.com/v10.0/" + env.account[accountName].accountId + "?fields=business_discovery.username(" + accountName + "){followers_count,media_count}&access_token=" + env.account[accountName].accessToken;
+
+  const options = {
+    url: URL,
+    method: 'GET'
+  };
+  request(options, function (error, response, body) {
+    const responseBody = JSON.parse(body);
+    const NowNumJson = {
+      "postNowNum": responseBody.business_discovery.media_count,
+      "followerNowNum": responseBody.business_discovery.followers_count
+    };
+    res.header('Content-Type', 'application/json; charset=utf-8')
+    res.send(NowNumJson);
+  });
+})
 
 
 module.exports = router;
--- routes/followers.js
+++ routes/followers.js
@@ -1,30 +1,48 @@
 const express = require('express');
+const app = express();
 const router = express.Router();
+const env = require('./../env.json')[app.get('env')];
+const request = require('request');
+
+// TODO:どこかでaccountNameを取得する
+const accountName = "test_t.saku";
+const URL = "https://graph.facebook.com/v10.0/" + env.account[accountName].accountId + "?fields=business_discovery.username(" + accountName + "){followers_count,media_count}&access_token=" + env.account[accountName].accessToken
 
 router.get('/', function (req, res, next) {
-  res.render('followers', {
-    accountName: 'test_t.saku',
-    post: {
-      nowNum: '39',
-      previousMonthOverNum: '8'
-    },
-    follower: {
-      nowNum: '4002',
-      previousMonthOverNum: '300'
-    },
-    likes: {
-      nowNum: '50',
-      previousMonthOverNum: '7'
-    },
-    comment: {
-      nowNum: '389',
-      previousMonthOverNum: '83'
-    },
-    engagement: {
-      nowNum: '5.42',
-      previousMonthOverNum: '0.78'
-    }
+  const options = {
+    url: URL,
+    method: 'GET'
+  }
+  request(options, function (error, response, body) {
+    const responseBody = JSON.parse(body);
+    const postNowNum = responseBody.business_discovery.media_count;
+    const followerNowNum = responseBody.business_discovery.followers_count;
+    console.log(postNowNum);
+
+    res.render('followers', {
+      accountName: accountName,
+      post: {
+        nowNum: postNowNum,
+        previousMonthOverNum: '8'
+      },
+      follower: {
+        nowNum: followerNowNum,
+        previousMonthOverNum: '300'
+      },
+      likes: {
+        nowNum: '50',
+        previousMonthOverNum: '7'
+      },
+      comment: {
+        nowNum: '389',
+        previousMonthOverNum: '83'
+      },
+      engagement: {
+        nowNum: '5.42',
+        previousMonthOverNum: '0.78'
+      }
+    });
   });
-});
+})
 
 module.exports = router;
