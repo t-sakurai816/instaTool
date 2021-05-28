954b45ca5f2a2e853460d6eec2dc939ee5427d19
t-sakurai816 <poppun_0816@icloud.com>
2021-05-28 17:40:58 +0900

    アカウント名を入力する

--- routes/followers.js
+++ routes/followers.js
@@ -5,44 +5,52 @@ const env = require('./../env.json')[app.get('env')];
 const request = require('request');
 
 // TODO:どこかでaccountNameを取得する
-const accountName = "test_t.saku";
-const URL = "https://graph.facebook.com/v10.0/" + env.account[accountName].accountId + "?fields=business_discovery.username(" + accountName + "){followers_count,media_count}&access_token=" + env.account[accountName].accessToken
+
 
 router.get('/', function (req, res, next) {
-  const options = {
-    url: URL,
-    method: 'GET'
-  }
-  request(options, function (error, response, body) {
-    const responseBody = JSON.parse(body);
-    const postNowNum = responseBody.business_discovery.media_count;
-    const followerNowNum = responseBody.business_discovery.followers_count;
-    console.log(postNowNum);
+  // URLからaccoundNameを読み込む
+  const accountName = req.query.accountName;
+  console.log("アカウント名：" + accountName);
+  // アカウント名が存在しているかどうか
+  if (Object.keys((env.account)).includes(accountName)) {//true
+    const URL = "https://graph.facebook.com/v10.0/" + env.account[accountName].accountId + "?fields=business_discovery.username(" + accountName + "){followers_count,media_count}&access_token=" + env.account[accountName].accessToken
+    const options = {
+      url: URL,
+      method: 'GET'
+    }
+    request(options, function (error, response, body) {
+      const responseBody = JSON.parse(body);
+      const postNowNum = responseBody.business_discovery.media_count;
+      const followerNowNum = responseBody.business_discovery.followers_count;
 
-    res.render('followers', {
-      accountName: accountName,
-      post: {
-        nowNum: postNowNum,
-        previousMonthOverNum: '8'
-      },
-      follower: {
-        nowNum: followerNowNum,
-        previousMonthOverNum: '300'
-      },
-      likes: {
-        nowNum: '50',
-        previousMonthOverNum: '7'
-      },
-      comment: {
-        nowNum: '389',
-        previousMonthOverNum: '83'
-      },
-      engagement: {
-        nowNum: '5.42',
-        previousMonthOverNum: '0.78'
-      }
+      res.render('followers', {
+        accountName: accountName,
+        post: {
+          nowNum: postNowNum,
+          previousMonthOverNum: 'XX'
+        },
+        follower: {
+          nowNum: followerNowNum,
+          previousMonthOverNum: 'XXX'
+        },
+        likes: {
+          nowNum: 'XX',
+          previousMonthOverNum: 'X'
+        },
+        comment: {
+          nowNum: 'XXX',
+          previousMonthOverNum: 'XX'
+        },
+        engagement: {
+          nowNum: 'X.XX',
+          previousMonthOverNum: 'X.XX'
+        }
+      });
     });
-  });
+  } else {
+    res.send('アカウント名が存在しません');
+  };
+
 })
 
 module.exports = router;
